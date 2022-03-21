d664390cf8025ff30622d50d8953d6f0f57b0a1a
Vinicius Matos de Mendonça <concursandopro1@gmail.com>
2022-03-21 15:11:05 -0300

    criando uma função p/ os 'produtos'

--- Construtores em JavaScript/criando_um_objeto.js
+++ Construtores em JavaScript/criando_um_objeto.js
@@ -1,4 +1,5 @@
-// Cria um objeto
+/* Step 1 */
+// Bóra criar um objeto
 let produto1 = {
   nome: "Blusa",
   preco: 120,
@@ -8,15 +9,35 @@ let produto1 = {
   },
 };
 
-var produto2 = {
+let produto2 = {
   nome: "Calça",
   preco: 300,
-  //Forma diferente de definir o método calculaDesconto()
+  //Forma diferente de definir o método calculaDesconto() com Object methods
   calculaDesconto: function () {
     //10% de desconto
     return this.preco * 0.1;
   },
 };
 
-console.log(produto1.calculaDesconto());
-console.log(produto2.calculaDesconto());
+// console.log(produto1.calculaDesconto());
+// console.log(produto2.calculaDesconto());
+
+/* Step 2 */
+//Pegaremos a estrutura comum dos produtos e colocaremos numa função criaProduto() para não repetirmos código
+function criaProduto(parametroNome, parametroPreco) {
+  return {
+    nome: parametroNome,
+    preco: parametroPreco,
+    calculaDesconto() {
+      return this.preco * 0.1;
+    },
+  };
+}
+
+let a1 = criaProduto("Blusa", 120);
+let a2 = criaProduto("Calça", 300)
+
+console.log("O nome do produto a1 é", a1.nome, "e teve um desconto de", a1.calculaDesconto(), "reais.")
+console.log(a2.calculaDesconto())
+
+
