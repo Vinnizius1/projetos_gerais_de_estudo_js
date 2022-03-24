3bc9143e1d9136411aac769d93391a7270ce2453
Vinicius Matos de Mendonça <concursandopro1@gmail.com>
2022-03-24 11:13:09 -0300

    refatoração no código

--- JS ES6+/desestruturacao.js
+++ JS ES6+/desestruturacao.js
@@ -11,26 +11,21 @@ const cliente = {
   },
 };
 
-const log = (dado) => console.log(dado);
-
-// log(cliente.nome); /* Vinicius Matos */
-// log(cliente.cpf); /* 011.111.111.11 */
-// log(cliente.endereco.pais); /* Brasil */
 
 /* 
-Agora vamos desestruturar o objeto "cliente"
+Vamos desestruturar o objeto "cliente"
 */
-const { nome, cpf, endereco } = cliente;
-// log(nome); /* Vinicius Matos */
-// log(cpf); /* 011.111.111.11 */
-// log(endereco); /* { rua: 'Avenida JS', cidade: 'Typora', estado: 'GO', pais: 'Brasil' } */
-// log(endereco.rua); /* Avenida JS */
+// const { nome, cpf, endereco } = cliente;
+// console.log(nome); /* Vinicius Matos */
+// console.log(cpf); /* 011.111.111.11 */
+// console.log(endereco); /* { rua: 'Avenida JS', cidade: 'Typora', estado: 'GO', pais: 'Brasil' } */
+// console.log(endereco.rua); /* Avenida JS */
 
-function clienteDesestruturado({ nome, cpf, endereco: { rua } }) {
-  log(nome); /* Vinicius Matos */
-  log(cpf); /* 011.111.111.11 */
-  log(endereco); /* { rua: 'Avenida JS', cidade: 'Typora', estado: 'GO', pais: 'Brasil' } */
-  log("Nome da rua: " + rua); /* Nome da rua: Avenida JS */
+function clienteDesestruturado({ nome, cpf, endereco }) {
+  console.log(nome); /* Vinicius Matos */
+  console.log(cpf); /* 011.111.111.11 */
+  console.log(endereco); /* { rua: 'Avenida JS', cidade: 'Typora', estado: 'GO', pais: 'Brasil' } */
+  console.log("Nome da rua: " + endereco.rua); /* Nome da rua: Avenida JS */
 }
 
 clienteDesestruturado(cliente);
