e49b285f8de41362d9e10c34eccc26722527da8c
vinicius <vmm_7@hotmail.com>
2022-05-06 22:13:47 -0300

    Função CREATE

--- CRUD em JavaScript/Nível 1 - JS puro/index.js
+++ CRUD em JavaScript/Nível 1 - JS puro/index.js
@@ -1,2 +1,33 @@
 /* FONTE: https://www.youtube.com/watch?v=5I4W0Mtcfqo&ab_channel=MarioSouto-DevSoutinho */
 
+/* [CRUD] JavaScript Básico */
+const miniTwitter = {
+  usuarios: [
+    {
+      username: "Vinicius",
+    },
+  ],
+  posts: [
+    {
+      id: 1,
+      owner: "Vinicius",
+      content: "Este é o primeiro tweet",
+    },
+  ],
+};
+
+// CREATE
+function criaPost(dados) {
+  miniTwitter.posts.push({
+    id: miniTwitter.posts.length + 1,
+    owner: dados.owner,
+    content: dados.content,
+  });
+}
+
+criaPost({ owner: "Pâmela", content: "Este é o segundo tweet" });
+console.log(miniTwitter)
+criaPost({ owner: "Chatosvaldo", content: "Este é o terceiro tweet" });
+
+criaPost({ owner: "Rubiatabata", content: "Este é o quarto tweet" });
+console.log(miniTwitter)
